0331cdc8c158cee47e2c12ebbb12b91e1333c513
Raveesh Avashia <raveeshav95@gmail.com>
2019-01-12 17:35:12 +0530

    Update carbon.py

--- carbon.py
+++ carbon.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import pylab as pl
 import numpy as np
-#%matplotlib inline
+import matplotlib.pyplot as plt
+from sklearn import linear_model, preprocessing
+from sklearn.metrics import r2_score
 
 # Read the FuelConsumption.csv file
 df = pd.read_csv('FuelConsumption.csv')
@@ -16,6 +16,7 @@ df.describe()
 # selecting a few attributes and features from the file 
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # plotting each of these features
 viz = cdf[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
@@ -47,16 +48,43 @@ plt.show()
 msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
 test = cdf[~msk]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Data Preprocessing using Feature Scaling 
+
+#RobustScaler() for outlier elimination
+rob_scaler = preprocessing.RobustScaler()
+rob_scaled_df = rob_scaler.fit_transform(cdf)
+
+#MinMaxScaler() for scaling feature to a given range
+mm_scaler = preprocessing.MinMaxScaler()
+mm_scaled_df = mm_scaler.fit_transform(cdf)
 
 # train data distribution
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+
+#C02EMISSION vs Enginesize
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
 
+#C02EMISSION vs Number of Cylinders
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='yellow')
+plt.xlabel("Cylinders")
+plt.ylabel("Emission")
+plt.show()
+
+#CO2EMISSION vs Fuel Consumption
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='green')
+plt.xlabel("Fuel Consumption")
+plt.ylabel("Emission")
+plt.show()
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # ML Linear Regression Algorithm
-from sklearn import linear_model
-regr = linear_model.LinearRegression()
+
+#Linear regression model 1 
+regr = linear_model.LinearRegression(fit_intercept=True, normalize=True, copy_X=True)
 train_x = np.asanyarray(train[['ENGINESIZE']])
 train_y = np.asanyarray(train[['CO2EMISSIONS']])
 regr.fit (train_x, train_y)
@@ -64,18 +92,61 @@ regr.fit (train_x, train_y)
 print ('Coefficients: ', regr.coef_)
 print ('Intercept: ',regr.intercept_)
 
-#  plot output
+#Linear regression model 2
+regr = linear_model.LinearRegression(fit_intercept=True, normalize=True, copy_X=True)
+train_x = np.asanyarray(train[['CYLINDERS']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+
+#Linear regression model 3
+regr = linear_model.LinearRegression(fit_intercept=True, normalize=True, copy_X=True)
+train_x = np.asanyarray(train[['FUELCONSUMPTION_COMB']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  plot output 1
 plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 
-# Final Evaluation
-from sklearn.metrics import r2_score
+#  plot output 2
+plt.scatter(train.CYLINDERS, train.CO2EMISSIONS,  color='green')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Cylinders")
+plt.ylabel("Emission")
 
+#  plot output 3
+plt.scatter(train.FUELCONSUMPTION_COMB, train.CO2EMISSIONS,  color='yellow')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Fuel Consumption")
+plt.ylabel("Emission")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Test for the linear model
+
+#Testing model for Case 1
 test_x = np.asanyarray(test[['ENGINESIZE']])
 test_y = np.asanyarray(test[['CO2EMISSIONS']])
 test_y_ = regr.predict(test_x)
+
+#Testing model for Case 2
+test_x = np.asanyarray(test[['CYLINDERS']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+#Testing model for Case 3
+test_x = np.asanyarray(test[['FUELCONSUMPTION_COMB']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Evalutation metrics
 
 print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
